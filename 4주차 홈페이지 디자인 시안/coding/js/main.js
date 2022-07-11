5a1ca5359a02dcf75973781bff2dae2145e4f2df
inseo <wisberry2022@gmail.com>
2022-07-11 20:54:32 +0900

    07/11 home upload

--- 4주차 홈페이지 디자인 시안/coding/js/main.js
+++ 4주차 홈페이지 디자인 시안/coding/js/main.js
@@ -1,4 +1,12 @@
 $(function () {
+  // color_set = ['#bae2ee', '#f6d1a4', '#c0022e']
+  $('main .main_slider').on('Init afterChange', function(e,s,c) {
+    console.log(e,s,c);
+    color_set = ['#bae2ee', '#f6d1a4', '#c0022e'];
+    $('.new_product .next_box').css('background', color_set[c]);
+    $('.new_product .prev_box').css('background', color_set[c]);
+  });
+
   $('#header_bg_video').YTPlayer({
     videoURL: 'https://youtu.be/LjRWdVdO50Y',
     containment: '#header',
