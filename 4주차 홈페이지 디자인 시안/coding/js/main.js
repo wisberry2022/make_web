75dc48566cfae9583b4af723d87c99ce74dd69b5
wisberry <wisberry2022@gmail.com>
2022-07-12 17:45:24 +0900

    07/12 upload

--- 4주차 홈페이지 디자인 시안/coding/js/main.js
+++ 4주차 홈페이지 디자인 시안/coding/js/main.js
@@ -1,10 +1,9 @@
 $(function () {
   // color_set = ['#bae2ee', '#f6d1a4', '#c0022e']
-  $('main .main_slider').on('Init afterChange', function(e,s,c) {
-    console.log(e,s,c);
+  $('main .main_slider').on('Init beforeChange', function (e, s, c) {
+    console.log(e, s, c);
     color_set = ['#bae2ee', '#f6d1a4', '#c0022e'];
-    $('.new_product .next_box').css('background', color_set[c]);
-    $('.new_product .prev_box').css('background', color_set[c]);
+    $('.new_product .next_box, .new_product .prev_box').css('background', color_set[(c + 1) < 3 ? c + 1 : c = 0]);
   });
 
   $('#header_bg_video').YTPlayer({
@@ -19,6 +18,8 @@ $(function () {
 
   $('main .new_product .main_slider').slick({
     arrows: false,
+    fade: true,
+    speed: 2000,
   });
 
   $('.new_product .prev_box').on('click', function () {
