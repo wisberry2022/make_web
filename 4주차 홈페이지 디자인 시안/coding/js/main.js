e0a1d811c517e5ca8305b6e9ac58cea1bd2e57dd
wisberry <wisberry2022@gmail.com>
2022-07-14 12:45:24 +0900

    modified

--- 4주차 홈페이지 디자인 시안/coding/js/main.js
+++ 4주차 홈페이지 디자인 시안/coding/js/main.js
@@ -1,9 +1,9 @@
 $(function () {
-  var s_cnt= 0;
-  
+  var s_cnt = 0;
+
   $('main .main_slider').on('Init beforeChange', function (e, s, c) {
     color_set = ['#bae2ee', '#f6d1a4', '#c0022e'];
-    num_set = [0,2,1];
+    num_set = [0, 2, 1];
     var cnt = s_cnt;
     console.log('before_slide: ', c);
     if (cnt <= 0) {
@@ -11,7 +11,7 @@ $(function () {
       cnt %= 3;
       console.log(cnt);
       $('.new_product .next_box, .new_product .prev_box').css('background', color_set[num_set[cnt]]);
-    }else if (cnt > 0) {
+    } else if (cnt > 0) {
       cnt %= 3;
       $('.new_product .next_box, .new_product .prev_box').css('background', color_set[cnt]);
     }
