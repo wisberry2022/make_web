445fac197abc83cf72f32295f0f946e1da666b96
inseo <wisberry2022@gmail.com>
2022-07-19 21:57:28 +0900

    07/19 upload

--- 5주차 홈페이지 디자인 시안/coding/js/main.js
+++ 5주차 홈페이지 디자인 시안/coding/js/main.js
@@ -1,5 +1,12 @@
 $(function () {
   $('#header .main_slider').slick({
     arrows: false,
+    dots: true,
   });
+
+  $(window).scroll(function() {
+    if ($(this).scrollTop() > 450) {
+      $('.brand_intro .img_box').addClass('show');
+    }
+  })
 })
