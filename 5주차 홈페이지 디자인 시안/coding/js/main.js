cc2c2dfcffdc65edfb7f5822cdf3bf6ad4351a90
wisberry <wisberry2022@gmail.com>
2022-07-20 17:52:04 +0900

    07/20 upload

--- 5주차 홈페이지 디자인 시안/coding/js/main.js
+++ 5주차 홈페이지 디자인 시안/coding/js/main.js
@@ -4,9 +4,16 @@ $(function () {
     dots: true,
   });
 
-  $(window).scroll(function() {
+  $(window).scroll(function () {
     if ($(this).scrollTop() > 450) {
       $('.brand_intro .img_box').addClass('show');
+      if ($(this).scrollTop() > 1600) {
+        $('main .sims .inner').addClass('hide');
+        setTimeout(function (e) {
+          $('main .btf').addClass('sticky');
+          $('main .sims').addClass('changeBackground');
+        }, 500);
+      }
     }
   })
 })
