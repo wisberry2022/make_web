02b8d1a5f0a91f53f88a4715a9c630641390dac2
wisberry <wisberry2022@gmail.com>
2022-08-02 14:52:27 +0900

    08/02 upload

--- 6주차 홈페이지 디자인 시안/coding/js/main.js
+++ 6주차 홈페이지 디자인 시안/coding/js/main.js
@@ -1,4 +1,5 @@
 $(function () {
+  // 동영상 재생하는 함수
   var PlayingYTP = function (playObj, src, containElem, ap_tf) {
     $(playObj).YTPlayer({
       videoURL: src,
@@ -13,7 +14,7 @@ $(function () {
   }
 
   // 현재 슬라이드 이외의 슬라이드를 모두 정지시키는 함수
-  var rotatingPause = function( playArr, c ) {
+  var rotatingPause = function (playArr, c) {
     playArr.forEach((elm, index) => {
       if (index != c) {
         $(elm).YTPPause();
@@ -22,8 +23,8 @@ $(function () {
   }
 
   // 웹 접속 시에 처음으로 실행되는 슬라이드 이외의 슬라이드를 모두 정지시키는 함수
-  var firstSettingYTP = function ( playArr ) {
-    playArr.forEach(function(elm, idx) {
+  var firstSettingYTP = function (playArr) {
+    playArr.forEach(function (elm, idx) {
       if (idx != 0) {
         $(elm).YTPPause();
         $(elm).YTPSeekTo(0);
@@ -33,17 +34,17 @@ $(function () {
   }
 
   var YTPArr = ['#bg_video', '#bg_video_02', '#bg_video_03'];
-  
+
   PlayingYTP('#bg_video', 'https://youtu.be/nDGv_jtZM0A', '.player01', true);
   PlayingYTP('#bg_video_02', 'https://youtu.be/GLkc8jtZGoU', '.player02', false);
   PlayingYTP('#bg_video_03', 'https://youtu.be/MdkFmOfRmNo', '.player03', false);
 
   firstSettingYTP(YTPArr);
-  
+
   $('.main_visual').on('beforeChange', function (e, s, c) {
     var YTPArr = ['#bg_video', '#bg_video_02', '#bg_video_03'];
-    rotatingPause(YTPArr, (c+1 <= 2 ? c+1 : 0 ) );
-    $(YTPArr[c ? (c-1>=0 ? c : 2) : 0]).YTPSeekTo(0);
+    rotatingPause(YTPArr, (c + 1 <= 2 ? c + 1 : 0));
+    $(YTPArr[c ? (c - 1 >= 0 ? c : 2) : 0]).YTPSeekTo(0);
   })
 
   $('.main_visual').on('afterChange', function (e, s, c) {
@@ -59,5 +60,10 @@ $(function () {
 
   $('.icon_menu .xi-search').on('click', function () {
     $('.gnb .search_box').toggleClass('on');
+  })
+
+  $('.icon_menu li:last-child').on('click', function (e) {
+    e.preventDefault();
+    $('.left_menu').toggleClass('show_on');
   })
 })
