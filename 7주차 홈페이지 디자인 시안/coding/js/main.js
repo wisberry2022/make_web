9414342b0c5291f067a592fb8f8b0fac3cb707b3
wisberry <wisberry2022@gmail.com>
2022-08-09 17:43:55 +0900

    08/09 upload

--- 7주차 홈페이지 디자인 시안/coding/js/main.js
+++ 7주차 홈페이지 디자인 시안/coding/js/main.js
@@ -18,9 +18,20 @@ $(function () {
     $('.product .left_slider>.prd_desc').eq(c).toggleClass('on').siblings().removeClass('on');
   });
 
-  $(window).on('scroll', function() {
-    if (($('.wb_intro').offset().top - $(this).scrollTop()) <= 100 ) {
+  $(window).on('scroll', function () {
+    if (($('.wb_intro').offset().top - $(this).scrollTop()) <= 100) {
       $('.wb_intro .container strong').fadeIn(1000);
+    }
+  });
+
+  $('.brand_box a').on('click', function () {
+    var idx = $(this).index();
+    $('.brand_wrap .each_brand').eq(idx).addClass('on').siblings().removeClass('on');
+  })
+
+  $(document).mouseup(function (e) {
+    if ($('.brand_wrap .each_brand').has(e.target).length == 0) {
+      $('.brand_wrap .each_brand').removeClass('on');
     }
   })
 
