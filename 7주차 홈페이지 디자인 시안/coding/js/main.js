15a563fbcd1c7db328ede89451852d0677cc07a5
wisberry <wisberry2022@gmail.com>
2022-08-10 17:43:13 +0900

    08/10 upload

--- 7주차 홈페이지 디자인 시안/coding/js/main.js
+++ 7주차 홈페이지 디자인 시안/coding/js/main.js
@@ -1,4 +1,10 @@
 $(function () {
+  // 원하는 위치로 스크롤 해주는 함수
+  function toScroll(toObj, time) {
+    var destination = $(toObj).offset().top;
+    $('html, body').animate({ scrollTop: destination }, time);
+  }
+
   $('header .main_slider').slick({
     arrows: false,
     dots: true,
@@ -7,6 +13,7 @@ $(function () {
 
   $('.product .slide_box .right_slider').slick({
     arrows: false,
+    autoplay: true,
     slidesToShow: 4,
   });
 
@@ -19,21 +26,40 @@ $(function () {
   });
 
   $(window).on('scroll', function () {
-    if (($('.wb_intro').offset().top - $(this).scrollTop()) <= 100) {
-      $('.wb_intro .container strong').fadeIn(1000);
+    if ((($('.wb_intro').offset().top - $(this).scrollTop()) <= 100) && (($('.wb_intro').offset().top - $(this).scrollTop()) >= -500)) {
+      $('.wb_intro .container .letter').fadeIn(1000);
+    } else {
+      $('.wb_intro .container .letter').fadeOut(1000);
     }
   });
+
+  $('.wb_intro .target').on('click', function () {
+    toScroll('.wb', 1000);
+  });
+
+  $('.wb_intro .container .letter').on('click', function () {
+    toScroll(`.wb>div:nth-child(${$(this).index('.letter') + 1})`, 1000)
+  })
 
   $('.brand_box a').on('click', function () {
     var idx = $(this).index();
     $('.brand_wrap .each_brand').eq(idx).addClass('on').siblings().removeClass('on');
+    $(this).addClass('on');
   })
 
   $(document).mouseup(function (e) {
     if ($('.brand_wrap .each_brand').has(e.target).length == 0) {
       $('.brand_wrap .each_brand').removeClass('on');
+      $('.brand_box a').removeClass('on');
     }
   })
 
+  $('.media_tab li').on('click', function () {
+    var idx = $(this).index();
+    $(this).addClass('on').siblings().removeClass('on');
+    $('.media .right').eq(idx).addClass('on').siblings().removeClass('on');
+
+
+  })
 
 })
