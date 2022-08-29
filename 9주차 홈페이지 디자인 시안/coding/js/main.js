369bd6a235cf9c3608f94e2db592257a01dc8930
wisberry <wisberry2022@gmail.com>
2022-08-29 17:49:36 +0900

    08/29 upload

--- 9주차 홈페이지 디자인 시안/coding/js/main.js
+++ 9주차 홈페이지 디자인 시안/coding/js/main.js
@@ -1,3 +1,56 @@
 document.addEventListener('DOMContentLoaded', () => {
+  function backgroundSlide() {
+    const HEADER = document.querySelector('#header');
+    const H_CON = document.querySelector('#header .container');
+    const LOGO = document.querySelector('#header .container h1 img');
+    const DESC = document.querySelectorAll('.program_desc');
+
+    let idx = 1;
+    let url_set = ['./assets/image/header/header_bg_01.jpg', './assets/image/header/header_bg_02.jpg',
+      './assets/image/header/header_bg_03.jpg'];
+
+    setInterval(function () {
+      if (idx == 2) {
+        H_CON.style.color = '#212121';
+        LOGO.setAttribute('src', "./assets/image/logo/mbc-logo-b.png")
+      } else {
+        H_CON.style.color = '';
+        LOGO.setAttribute('src', "./assets/image/logo/mbc-logo.png");
+      }
+      DESC.forEach((elm, index) => {
+        elm.classList.remove('on');
+        if (index == idx) {
+          elm.classList.add('on');
+        }
+
+      })
+      HEADER.style.background = `linear-gradient(to bottom, rgba(5, 18, 26, 0) 0, rgba(33, 33, 33, 1) 90%), url(${url_set[idx]})`;
+      idx < 2 ? idx++ : idx = 0;
+
+    }, 3000);
+  }
+
+  backgroundSlide();
+
+  const BROAD_TAB = document.querySelector('.category_list');
+  const CONTENTS_TAB = document.querySelectorAll('.contents_box');
+  let BROAD_LIST = [...BROAD_TAB.children];
+
+  console.log(CONTENTS_TAB);
+
+  BROAD_TAB.addEventListener('click', (event) => {
+    if (event.target != event.currentTarget) {
+      console.log(event.target);
+      BROAD_LIST.forEach((elm) => {
+        elm.classList.remove('on');
+        CONTENTS_TAB.forEach((el) => {
+          el.classList.remove('on');
+        })
+      })
+      // BROAD_LIST.forEach()
+      event.target.parentNode.classList.add('on');
+    }
+  });
 
 });
+
