0aff0bfd6453543cf840ea4e706a1dbc70273846
inseo <wisberry2022@gmail.com>
2022-09-07 20:58:38 +0900

    09/07 upload

--- 10주차 홈페이지 디자인 시안/coding/jungto/src/footer/Footer.js
+++ 10주차 홈페이지 디자인 시안/coding/jungto/src/footer/Footer.js
@@ -11,7 +11,7 @@ const TopFooter = () => {
       <div className="container">
         <ul className="top_list">
           {list_arr.map((el, idx) => (
-            <Link to="/"><li key={idx}><span>{el}</span><i className="xi-angle-right"></i></li></Link>
+            <Link to="/" className="list"><li key={idx}><span>{el}</span><i className="xi-angle-right"></i></li></Link>
           ))}
         </ul>
       </div>
@@ -20,9 +20,35 @@ const TopFooter = () => {
 }
 
 const BottomFooter = () => {
+  const mini_list = ['개인정보 처리방침', '전화번호안내', '오시는 길'];
+  const sns_list = ['xi-facebook-official', 'xi-twitter', 'xi-instagram'];
+
   return (
     <div className="bottom_ft">
-      hello
+      <div className="container">
+        <h1>
+          <Link to="/">
+            <img src="./logo.png" alt="hello" />
+          </Link>
+        </h1>
+        <ul className="info">
+          {mini_list.map((elm, idx) => (
+            <Link to="/" className="info_list"><li key={idx}>{elm}</li></Link>
+          ))}
+        </ul>
+        <div className="bottom_list">
+          <p>
+            정토회 홈페이지의 모든 콘텐츠에 대한 저작권은 정토회에 있습니다.<br />
+            무단도용 및 2차 가공, 복제, 공중송신 등을 금지하며 저작권 침해 시 민,형사상 책임이 따를 수 있음을 알려드립니다.
+          </p>
+          <ul className="sns_set">
+            {sns_list.map((el, idx) => (
+              <Link to="/" className="sns_icon"><li key={idx}><i className={el}></i></li></Link>
+            ))}
+          </ul>
+        </div>
+        <span>Copyright&copy; jungto Order of Korean Buddhism All right reserved.</span>
+      </div>
     </div>
   )
 }
